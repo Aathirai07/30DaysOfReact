4554ba47ccbbb01a2f29a6b5c2d0928b1949bfca
Aathirai Kumanan <kaathirai@athenahealth.com>
2020-06-06 17:12:34 +0530

    Day 12

---
+++ Day12/30days/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from "./components/Header";
+import Content from "./components/Content";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Day12/30days/src/components/Content.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export class Content extends React.Component {
+    render() {
+        return <p className="App-intro">Content goes here</p>;
+    }
+}
+export default Content;
