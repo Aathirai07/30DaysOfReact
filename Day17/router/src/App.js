7b58db3fb35a032c4cfcf124eecab9c92e18951b
Aathirai Kumanan <kaathirai@athenahealth.com>
2020-06-08 20:01:29 +0530

    Router

--- Day17/router/src/App.js
+++ Day17/router/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+const Home = () => (
+  <div>
+    <h1>Welcome home</h1>
+  </div>
+);
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <Link to="/">Go home</Link>
+  </div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            path="/about"
+            render={renderProps => (
+              <div>
+                <Link to="/about/ari">Ari</Link>
+                <Link to="/about/nate">Nate</Link>
+                <Route
+                  path="/about/:name"
+                  render={renderProps => (
+                    <div>
+                      <About name={renderProps.match.params.name} />
+                      <Link to="/">Go home</Link>
+                    </div>
+                  )}
+                />
+              </div>
+            )}
+          />
+          <Route
+            path="/"
+            render={renderProps => (
+              <div>
+                Home is underneath me
+                <Home {...this.props} {...renderProps} />
+              </div>
+            )}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
-
 export default App;
